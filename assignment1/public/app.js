4d5af2ac062eed92a6c27d2f1f595c9f158c789c
Shubham Deshpande <shubhamdesh11153@gmail.com>
2020-01-29 23:02:40 -0800

    Updated app.jsx

--- assignment1/public/app.js
+++ assignment1/public/app.js
@@ -1,4 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-const element = React.createElement("div", null, React.createElement("div", null, React.createElement("h1", null, "Shubham Deshpande")), React.createElement("div", null, React.createElement("h1", null, "Photo")), React.createElement("div", null, React.createElement("h1", null, "GitHub Link")));
-ReactDOM.render(element, document.getElementByTagName('body'));
+const element = React.createElement("div", null, React.createElement("div", null, React.createElement("h1", null, "Shubham Deshpande")), React.createElement("div", null, React.createElement("img", {
+  src: "img01.jpg",
+  alt: "My Photo",
+  width: "300",
+  height: "300"
+}), React.createElement("br", null), React.createElement("br", null)), React.createElement("div", null, "Always Smiling :), honest and helpful ! ", React.createElement("br", null), "Well that is an informal introduction, let us talk about something technical ", React.createElement("br", null), "Have 2 years of experience in software development ", React.createElement("br", null), "Slightly biased towards using Java, but always ready to work on any new technology ", React.createElement("br", null), "Love web development ", React.createElement("br", null), "And yes playing video games and cricket are some of the many hobbies.", React.createElement("br", null), React.createElement("br", null)), React.createElement("div", null, React.createElement("a", {
+  href: "https://github.com/deshpandeshubham"
+}, "VIEW MY GITHUB REPO")));
+ReactDOM.render(element, document.getElementById('root'));
--- assignment1/src/app.jsx
+++ assignment1/src/app.jsx
@@ -1,13 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom';
-
 const element = (
     <div>
         <div>
             <h1>Shubham Deshpande</h1>
         </div>
         <div>
-            <img src="images/img01.jpg" alt="My Photo" width="500" height="500"></img>
+            <img src="img01.jpg" alt="My Photo" width="300" height="300"></img><br/><br/>
         </div>
         <div>
             Always Smiling :), honest and helpful ! <br/>
@@ -15,7 +12,7 @@ const element = (
             Have 2 years of experience in software development <br/>
             Slightly biased towards using Java, but always ready to work on any new technology <br/>
             Love web development <br/>
-            And yes playing video games and cricket are some of the many hobbies.
+            And yes playing video games and cricket are some of the many hobbies.<br/><br/>
         </div>
         <div>
             <a href="https://github.com/deshpandeshubham">VIEW MY GITHUB REPO</a>
@@ -23,4 +20,4 @@ const element = (
     </div>
 )
    
-ReactDOM.render(element, document.getElementByTagName('body'));
+ReactDOM.render(element, document.getElementById('root'));
